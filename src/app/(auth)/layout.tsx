c620f4dcaa7857f834eb71ce7bb0ce748ef5c358
VovaStepanov <vovastepanov741@gmail.com>
2024-04-28 13:36:07 +0300

    fix: add header to auth and fix styles

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,23 @@
+import { HeartHandshake } from "lucide-react";
+import Link from "next/link";
+
 interface AuthLayoutPropsType {
     children: React.ReactNode;
 }
 
 const AuthLayout: React.FC<AuthLayoutPropsType> = ({ children }) => {
-    return <>{children}</>;
+    return (
+        <>
+            <header className="p-4 fixed top-0">
+                <Link href="/" className="flex gap-1">
+                    <HeartHandshake className="w-6 h-6" />
+                    <span className="font-medium">Back</span>
+                    <span className="sr-only">Logo</span>
+                </Link>
+            </header>
+            <main>{children}</main>
+        </>
+    );
 };
 
 export default AuthLayout;
